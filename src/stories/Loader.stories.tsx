9bc1814de7e513792cffe5e81ecdf6015c9a5436
Federico Baldini <federicobaldini@MBP-di-Federico.fritz.box>
2021-09-03 01:17:22 +0200

    update story

--- src/stories/Loader.stories.tsx
+++ src/stories/Loader.stories.tsx
@@ -24,12 +24,7 @@ export default {
   },
 } as ComponentMeta<typeof Loader>;
 
-const Template: ComponentStory<typeof Loader> = (args) => (
-  <div style={{ display: "flex", flexDirection: "column" }}>
-    <Loader />;
-    <Loader variant={Variant.CircleDots} />;
-  </div>
-);
+const Template: ComponentStory<typeof Loader> = (args) => <Loader {...args} />;
 
 export const Default = Template.bind({});
 Default.args = {
